7550eae1625167575b7b6e016199c68deff02ffc
nayaib98 <nayaiburgess@gmail.com>
2019-01-31 12:11:50 -0500

    working

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Shop from './Shop';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome Home</h1>
+
+                <Link to= {Shop}>
+                <button>Shop Cars</button>
+                </Link>
             </div>
         );
     }
